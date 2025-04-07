d624055f4ea3ef9d170cc7191b95fdc7d2ee8dc5
shop <pepigames@outlook.es>
2025-04-07 23:53:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public')); // tu carpeta con HTML/CSS/JS
+
+io.on('connection', (socket) => {
+  console.log('Un usuario se conectó');
+
+  socket.on('mensaje', (data) => {
+    io.emit('mensaje', data); // reenviar a todos
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Un usuario se desconectó');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Servidor en http://localhost:3000');
+});
 
